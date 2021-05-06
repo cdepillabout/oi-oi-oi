a6f106bda7d211eddb39a973bddf1a04ac974bef
Dennis Gosnell <cdep.illabout@gmail.com>
2021-05-06 22:28:51 +0900

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE InstanceSigs #-}
@@ -31,10 +32,13 @@ stdOI :: OI a
 stdOI = OI ()
 
 coPure :: b -> OI a -> b
-coPure b _ = b
+coPure b (D _) = b
+coPure _ _ = error "coPure: passed OI"
+{-# NOINLINE coPure #-}
 
 coOpenFile :: FilePath -> IOMode -> OI a -> Handle
-coOpenFile s m _ = unsafePerformIO $ openFile s m
+coOpenFile s m (D _) = unsafePerformIO $ openFile s m
+coOpenFile _ _ _ = error "coOpenFile: passed OI"
 {-# NOINLINE coOpenFile #-}
 
 coHGetChar :: OI Handle -> Char
@@ -74,7 +78,12 @@ echoFunc s t =
   t =>>
   coOpenFile s ReadMode =>>
   coHGetChar =>>
+  coPutChar =>>
+  coPure '\n' =>>
   coPutChar
+
+runEchoFunc :: IO ()
+runEchoFunc = run (echoFunc "oi-oi-oi.cabal")
 
 echoFuncDoesn'tWork :: OI a -> OI a
 echoFuncDoesn'tWork t = t
